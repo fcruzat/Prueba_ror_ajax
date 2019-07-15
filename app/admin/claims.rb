01414517149a9e44bf57787827c19696fd8514b0
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-07-14 22:42:30 -0400

    Últimos pequeños cambios

--- app/admin/claims.rb
+++ app/admin/claims.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Claim do
     #
     # permit_params :list, :of, :attributes, :on, :model
     #
-    permit_params :content
+    permit_params :content, :user_id
     # or
     #
     # permit_params do
@@ -13,4 +13,12 @@ ActiveAdmin.register Claim do
     #   permitted << :other if params[:action] == 'create' && current_user.admin?
     #   permitted
     # end
+    form do |f|
+        f.inputs do
+            f.input :user, collection: User.all.pluck(:email, :id)
+            f.input :company, collection: Company.all.pluck(:name)
+            f.input :content
+        end
+        actions
+    end
 end
